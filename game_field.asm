a0f88119e38e542eb90ee51f4a258aab8750b4ba
Tim Grable <tgrable@acculynx.com>
2019-03-09 13:48:07 -0600

    sprites

--- game_field.asm
+++ game_field.asm
@@ -1,9 +1,90 @@
-.importonce
+#importonce
 
-// init_game_field:
-//     lda #147
-//     jsr kernal_chrout
+init_game_field:
+    lda #147
+    jsr kernal_chrout
 
-//     ldx #$00            // set x to zero
-//     stx $d021           // background color
-//     stx $d020           // border color
+    ldx #$00            // set x to zero
+    stx $d021           // background color
+    stx $d020           // border color
+
+    :draw_line(top_line, top_character)
+    :draw_line(bottom_line, bottom_character)
+    :split_screen()
+    rts
+
+.macro draw_line(start, character) {
+    ldx #$00
+    lda #$78            //character character is giving me the wrong char code  // #$20 is the spacebar Screen Code
+draw_loop:
+    sta start,x         // fill four areas with 256 spacebar characters
+    cpx #39
+    beq end_draw_loop       // did X turn to zero yet?
+    inx
+    jmp draw_loop
+end_draw_loop:
+}
+
+.macro split_screen() {
+    ldx #$00 
+    ldy #$00               
+    lda #$78
+    sta $043B,x 
+clear_screen:
+    cpx #$28
+    beq draw_dot                 
+    inx
+    iny
+    beq end
+    jmp clear_screen       
+draw_dot:
+    sta $043B,y
+    sta $052B,y 
+    sta $061B,y 
+    sta $06E3,y 
+    ldx #$00
+    jmp clear_screen
+end:
+}
+
+.macro draw_score() {
+    ldx #$79
+    stx 1119
+    stx 1120
+    stx 1121
+
+    ldx #$75
+    stx 1159
+    stx 1199
+    stx 1239
+
+    ldx #$76
+    stx 1161
+    stx 1201
+    stx 1241
+
+    ldx #$78
+    stx 1279
+    stx 1280
+    stx 1281
+
+    ldx #$79
+    stx 1125
+    stx 1126
+    stx 1127
+
+    ldx #$75
+    stx 1165
+    stx 1205
+    stx 1245
+
+    ldx #$76
+    stx 1167
+    stx 1207
+    stx 1247
+
+    ldx #$78
+    stx 1285
+    stx 1286
+    stx 1287
+}
--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-.import source "game_field.asm"
+#import "game_field.asm"
 
 *=$0801
 
@@ -15,38 +15,36 @@
 .label bottom_character = $0079
 
 .namespace sprites {
-    .label postition = VIC2
+    .label postition    = VIC2
     .label enabled_bits = VIC2 + 21
+    .label colors = VIC2 + 39
+    .label pointers = top_line + 1024 - 8
+    .label vertical_stretch_bits = VIC2 + 23
 }
 
 main:
     jsr init_game_field
 
-init_game_field:
-    lda #147
-    jsr kernal_chrout
-
-    ldx #$00            // set x to zero
-    stx $d021           // background color
-    stx $d020           // border color
-
-    :draw_line(top_line, top_character)
-    :draw_line(bottom_line, bottom_character)
-    :split_screen()
     :draw_score()
 
 draw_sprite:
     lda #%00000001
     sta sprites.enabled_bits
+    sta sprites.vertical_stretch_bits
+    lda #$04
+    sta sprites.colors
 
     .var x = 25
-    .var y = 51
+    .var y = 55
 
     lda #x
     sta sprites.postition + 0
     lda #y
     sta sprites.postition + 1
 
+    lda #254
+    sta sprites.pointers
+    
 game_loop:
     // :handle_joystick_input(joystick_port_1)
     // :draw_player()
@@ -54,89 +52,58 @@ game_loop:
 
 end:
 
-.macro draw_line(start, character) {
-    ldx #$00
-    lda #$78            //character character is giving me the wrong char code  // #$20 is the spacebar Screen Code
-draw_loop:
-    sta start,x         // fill four areas with 256 spacebar characters
-    cpx #39
-    beq end_draw_loop       // did X turn to zero yet?
-    inx
-    jmp draw_loop
-end_draw_loop:
+
+.pc = 64*254
+:sprite_row(%000000000000000000000000)
+:sprite_row(%000000000000000000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000111111000000000)
+:sprite_row(%000000000000000000000000)
+:sprite_row(%000000000000000000000000)
+// :sprite_row(%000011000000000111111000)
+// :sprite_row(%011011000000000110000000)
+// :sprite_row(%011111000000000111111000)
+// :sprite_row(%011001100000000000011000)
+// :sprite_row(%011001100000000111111000)
+// :sprite_row(%011111100000000110011000)
+// :sprite_row(%011111100000000110011000)
+// :sprite_row(%011000000000000110011000)
+// :sprite_row(%011111100000000111111000)
+// :sprite_row(%000001100000000111111000)
+// :sprite_row(%011111100000000110000000)
+// :sprite_row(%011111100000000110000000)
+// :sprite_row(%000110000000000111111000)
+// :sprite_row(%000110000000000110001100)
+// :sprite_row(%000110000000000110011000)
+// :sprite_row(%000110000000000111100000)
+// :sprite_row(%000110000000000110011000)
+// :sprite_row(%011011000000000111111000)
+// :sprite_row(%011011000000000110000000)
+// :sprite_row(%011111000000000111111000)
+// :sprite_row(%011011000000000111111000)
+
+
+.macro sprite_row(value) {
+  .byte extract_byte(value, 2), extract_byte(value, 1), extract_byte(value, 0) 
 }
 
-.macro split_screen() {
-    ldx #$00 
-    ldy #$00               
-    lda #$78
-    sta $043B,x 
-clear_screen:
-    cpx #$28
-    beq draw_dot                 
-    inx
-    iny
-    beq end
-    jmp clear_screen       
-draw_dot:
-    sta $043B,y
-    sta $052B,y 
-    sta $061B,y 
-    sta $06E3,y 
-    ldx #$00
-    jmp clear_screen
-end:
-}
-
-.macro draw_score() {
-    ldx #$79
-    stx 1119
-    stx 1120
-    stx 1121
-
-    ldx #$75
-    stx 1159
-    stx 1199
-    stx 1239
-
-    ldx #$76
-    stx 1161
-    stx 1201
-    stx 1241
-
-    ldx #$78
-    stx 1279
-    stx 1280
-    stx 1281
-
-    ldx #$79
-    stx 1125
-    stx 1126
-    stx 1127
-
-    ldx #$75
-    stx 1165
-    stx 1205
-    stx 1245
-
-    ldx #$76
-    stx 1167
-    stx 1207
-    stx 1247
-
-    ldx #$78
-    stx 1285
-    stx 1286
-    stx 1287
-
-    // lda #$01
-    // sta 55391
-    // sta 55392
-    // sta 55393
-
-
-    // lda #$00
-    // sta 1024
-    // lda #$01
-    // sta 55296
+.function extract_byte(value, byte_id) {
+  .var bits = byte_id * 8
+  .eval value = value >> bits
+  .return value & 255
 }
